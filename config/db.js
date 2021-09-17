4594e23828fec0833b9d9742264fa923991b3533
rcsbotimpacta <rcsbotimpacta@gmail.com>
2021-09-16 21:40:19 -0300

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://richardcs:1234@blogapp-prod.tnlo3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
+    module.exports = {MONGODB_URI: "mongodb+srv://richardcs:1234@blogapp-prod.tnlo3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
 }else{
-    module.exports = {mongoURI: "mongodb://localhost/blogapp"}
+    module.exports = {MONGODB_URI: "mongodb://localhost/blogapp"}
 
 }
